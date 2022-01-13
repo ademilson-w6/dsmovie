58dfb0b29dafd7290283e155b37f0f2e5f24abb3
Ademilson Silva <ademilson.w6@hotmail.com>
2022-01-12 23:39:20 -0300

    Domain model, database seed

---
+++ dsmovie/src/main/java/com/example/dsmovie/entities/User.java
@@ -0,0 +1,42 @@
+package com.example.dsmovie.entities;
+
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private Long id;
+     private String email;
+
+    public User(Long id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+    public User() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
